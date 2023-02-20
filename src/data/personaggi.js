c9f932184873e725fb7b2f6d1e41a69da039098d
Cicciogamero <cag.vedano@gmail.com>
2023-02-20 12:10:41 +0100

    Lista personaggi e CardPersonaggio

---
+++ src/data/personaggi.js
@@ -0,0 +1,49 @@
+const personaggi = [
+    {
+        id: 1,
+        name: "Miðgarðsormr",
+        avatar: "dragonidebarbaro.webp",
+        race:"dragonide",
+        class:"barbaro",
+        gender:"maschio",
+        background:"eremita"
+    },
+    {
+        id: 2,
+        name: "Jog",
+        avatar: "changeling.webp",
+        race:"changeling",
+        class:"paladino",
+        gender:"maschio",
+        background:"accolito"
+    },
+    {
+        id: 3,
+        name: "Duolingo",
+        avatar: "duolingomacho.webp",
+        race:"kenku",
+        class:"barbaro",
+        gender:"maschio",
+        background:"saggio"
+    },
+    {
+        id: 4,
+        name: "Gatto con gli stivali",
+        avatar: "gatitoconglistivali.jpg",
+        race:"gatto",
+        class:"paladino",
+        gender:"maschio",
+        background:"ladro"
+    },
+    {
+        id: 5,
+        name: "Predator",
+        avatar: "predator.webp",
+        race:"alieno",
+        class:"guerriero",
+        gender:"maschio",
+        background:"eremita"
+    },
+];
+
+export { personaggi };
